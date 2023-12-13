7454c586bdc54be448671f8145892cd90dd7cbfa
박준영 <wnsdud6969@naver.com>
2023-12-13 19:09:27 +0900

    feat(Lottos): 한 가지 기능을 구현하다
    - 발행된 로또들의 순위를 계산한다

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,5 +16,19 @@ public class Lottos {
         return lottos.stream()
             .map(lotto -> lotto.getNumbers())
             .collect(Collectors.toList());
+    }
+
+    public List<Rank> calculateRankOfLottos(Lotto winningLotto, LottoNumber bonusNumber) {
+        List<Rank> ranks = new ArrayList<>();
+
+        for (Lotto lotto : lottos) {
+            int matchCount = lotto.countMatchWith(winningLotto);
+            boolean hasBonusNumber = lotto.hasNumber(bonusNumber);
+            Rank rank = Rank.findRank(matchCount, hasBonusNumber);
+
+            ranks.add(rank);
+        }
+
+        return ranks;
     }
 }
