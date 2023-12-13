03a1b2185978517ba86bc01f34c89d4f88dea1a6
박준영 <wnsdud6969@naver.com>
2023-12-13 19:10:28 +0900

    feat(OutputView): 한 가지 기능을 구현하다
    - 당첨 통계를 출력한다
    - 수익률을 출력한다
    - 예외 메세지를 출력한다

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,14 @@
 package lotto.view;
 
 import java.util.List;
+import java.util.Map.Entry;
+import lotto.domain.Rank;
+import lotto.domain.Statistics;
 
 public class OutputView {
+
+    private static final String MESSAGE_WITHOUT_BONUS_NUMBER = "%d개 일치 (%,d원) - %d개";
+    private static final String MESSAGE_WITH_BONUS_NUMBER = "%d개 일치, 보너스 볼 일치 (%,d원) - %d개";
 
     private OutputView() {
 
@@ -14,5 +20,46 @@ public class OutputView {
         for (String lottoInformation : issuedLottosInformation) {
             System.out.println(lottoInformation);
         }
+    }
+
+    public static void printStatistics(Statistics statistics) {
+        for (Entry<Rank, Integer> rank : statistics.getStatistics()) {
+            if (rank.getKey().equals(Rank.NOTHING)) {
+                continue;
+            }
+
+            if (rank.getKey().equals(Rank.SECOND)) {
+                printMessageWithBonusNumber(rank);
+                continue;
+            }
+
+            printMessageWithoutBonusNumber(rank);
+        }
+    }
+
+    private static void printMessageWithBonusNumber(Entry<Rank, Integer> rank) {
+        System.out.println(String.format(
+            MESSAGE_WITH_BONUS_NUMBER,
+            rank.getKey().getMatchCount(),
+            rank.getKey().getPrize().getMoney(),
+            rank.getValue()
+        ));
+    }
+
+    private static void printMessageWithoutBonusNumber(Entry<Rank, Integer> rank) {
+        System.out.println(String.format(
+            MESSAGE_WITHOUT_BONUS_NUMBER,
+            rank.getKey().getMatchCount(),
+            rank.getKey().getPrize().getMoney(),
+            rank.getValue()
+        ));
+    }
+
+    public static void printProfit(double profit) {
+        System.out.println(String.format("총 수익률은 %.1f%%입니다.", profit));
+    }
+
+    public static void printExceptionMessage(IllegalArgumentException illegalArgumentException) {
+        System.out.println(illegalArgumentException.getMessage());
     }
 }
