516daba864bf5a7727fe9ecb0302d549c0fa0348
박준영 <wnsdud6969@naver.com>
2023-12-13 17:06:55 +0900

    feat(LottoMachine): 두 가지 기능을 구현하다
    - 로또 구입 금액이 유효한지 검증한다
    - 로또 구입 금액에 맞게 로또를 발행한다

---
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoMachine {
+
+    private static final Money LOTTO_PRICE = new Money(1000);
+
+    private LottoMachine() {
+
+    }
+
+    public static Lottos issueLottos(Money lottoPurchaseMoney) {
+        validate(lottoPurchaseMoney);
+        int numberOfLottos = lottoPurchaseMoney.divideBy(LOTTO_PRICE);
+        return new Lottos(issueLottos(numberOfLottos));
+    }
+
+    private static List<Lotto> issueLottos(int numberOfLottos) {
+        return IntStream.range(0, numberOfLottos)
+            .mapToObj(i -> new Lotto(generateRandomNumbers()))
+            .collect(Collectors.toList());
+    }
+
+    private static void validate(Money lottoPurchaseMoney) {
+        if (!lottoPurchaseMoney.isDivideUpBy(LOTTO_PRICE)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (lottoPurchaseMoney.divideBy(LOTTO_PRICE) == 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static List<Integer> generateRandomNumbers() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        randomNumbers.sort(Comparator.naturalOrder());
+        return randomNumbers;
+    }
+}
